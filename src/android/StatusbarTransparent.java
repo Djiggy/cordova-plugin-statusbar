be7f0531d135a022ed27a7df9d50b1b9afc938dc
Nikolay Babanov <ni.babanov@gmail.com>
2016-07-04 17:38:31 +0300

    Added method to check if the plugin is supported

--- src/android/StatusbarTransparent.java
+++ src/android/StatusbarTransparent.java
@@ -12,34 +12,55 @@ import android.view.WindowManager.LayoutParams;
 import android.graphics.Color;
 import android.view.Window;
 
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
+
 public class StatusbarTransparent extends CordovaPlugin {
 
     @Override
     protected void pluginInitialize() {
-
-        cordova.getActivity().runOnUiThread( new Runnable() {
-            public void run() {
-                Window window = cordova.getActivity().getWindow();
-                window.addFlags(LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-                window.clearFlags(LayoutParams.FLAG_TRANSLUCENT_STATUS);
-                window.setStatusBarColor(Color.argb(0, 0, 0, 0));
-            }
-        });
+        if(VERSION.SDK_INT >= VERSION_CODES.KITKAT) {
+            cordova.getActivity().runOnUiThread( new Runnable() {
+                public void run() {
+                    Window window = cordova.getActivity().getWindow();
+                    window.addFlags(LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                    window.clearFlags(LayoutParams.FLAG_TRANSLUCENT_STATUS);
+                    window.setStatusBarColor(Color.argb(0, 0, 0, 0));
+                }
+            });
+        }
     }
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callback) throws JSONException {
         // grab the correct methods
         if ("getStatusbarHeight".equalsIgnoreCase(action)) {
-            Rect rectangle = new Rect();
-            Window window = cordova.getActivity().getWindow();
-            window.getDecorView().getWindowVisibleDisplayFrame(rectangle);
-            int statusBarHeight = rectangle.top;
-
-            callback.success(statusBarHeight);
+            getStatusBarHeight(callback);
+            return true;
+        } else if ("isSupported".equalsIgnoreCase(action)) {
+            isSupported(callback);
             return true;
         } else {
             callback.error("Unknown Action: " + action);
+            return false;
+        }
+    }
+
+    private void getStatusBarHeight(CallbackContext callback) {
+        Rect rectangle = new Rect();
+        Window window = cordova.getActivity().getWindow();
+        window.getDecorView().getWindowVisibleDisplayFrame(rectangle);
+        int statusBarHeight = rectangle.top;
+
+        callback.success(statusBarHeight);
+    }
+
+    private boolean isSupported(CallbackContext callback) {
+        if(VERSION.SDK_INT >= VERSION_CODES.KITKAT) {
+            callback.success(true);
+            return true;
+        } else {
+            callback.success(false);
             return false;
         }
     }
--- www/statusbar.js
+++ www/statusbar.js
@@ -6,4 +6,8 @@ StatusbarTransparent.prototype.getStatusbarHeight = function (success, fail) {
 	cordova.exec(success ? success : null, fail ? fail : null, 'StatusbarTransparent', 'getStatusbarHeight', []);
 };
 
+StatusbarTransparent.prototype.isSupported = function (success, fail) {
+	cordova.exec(success ? success : null, fail ? fail : null, 'StatusbarTransparent', 'isSupported', []);
+};
+
 module.exports = new StatusbarTransparent();
